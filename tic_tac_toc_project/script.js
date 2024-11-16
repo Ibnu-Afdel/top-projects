c78340d264aa6c63fd758a37fbb5ac13e7ab79f0
Ibnu Afdel <ibnuafdel@gmail.com>
2024-11-16 19:07:35 +0300

    Add the ability to make move, Add player function

--- tic_tac_toc_project/script.js
+++ tic_tac_toc_project/script.js
@@ -1,5 +1,21 @@
 const Gameboard = (() => {
     const board = ['', '', '', '', '', '', '', '', ''];
     const getBoard = () => board ;
-    return {getBoard}
-}) ();
+    
+    const makeMove = function (index, mark){
+        if (board[index] === ''){
+            board[index] = mark ;
+            return true
+        } else {
+            return false
+        }
+    } 
+    return {getBoard, makeMove}
+})();
+
+const Player = function(name, mark){
+    return {name, mark}
+}
+
+const playerX = Player('Player X', 'X')
+const playerY = Player('Player Y', 'Y')
