f87d036288453f9d85ad1b7260b140000148bf66
Ibnu Afdel <ibnuafdel@gmail.com>
2024-12-23 22:56:25 +0300

    feat: Show avatar and bio aswell

--- github-info/script.js
+++ github-info/script.js
@@ -9,10 +9,19 @@ async function fetchData(){
         const userData = await responseFromGithub.json();
         console.log(userData);
 
+        const img = document.createElement('img');
+        img.src = userData.avatar_url;
+        document.body.appendChild(img)
+
         const name = document.createElement('h2');
         name.textContent = userData.name
 
         document.body.appendChild(name)
+
+        const bio = document.createElement('p');
+        bio.textContent = userData.bio
+
+        document.body.appendChild(bio)
         
     } catch(error){
         console.log(`Error : ${error}`);
